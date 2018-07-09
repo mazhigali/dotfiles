344054ed08f8db479e3d3eccb7d3f873ff21de39
mazhigali <mazhigali@gmail.com>
2018-07-09 14:24:47 +0300

    3

--- init.sh
+++ init.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-yum -y install zsh, mc, git, curl, neovim, tmux
+yum -y install zsh mc git curl neovim tmux
 $ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 git clone https://github.com/mazhigali/dotfiles.git
 cd dotfiles
+mkdir $HOME/.config/nvim
+rm .zshrc
 
 ln -s .zshrc       $HOME/.zshrc
 ln -s init.vim       $HOME/.config/nvim/init.vim
