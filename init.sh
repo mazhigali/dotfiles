807316dcfeae421499d2313b5db9864c99e93ef1
mazhigali <mazhigali@gmail.com>
2018-07-09 20:13:41 +0300

    3

--- init.sh
+++ init.sh
@@ -4,6 +4,7 @@ export DOTFILES=$HOME/dotfiles
 
 yum -y install zsh mc git curl neovim tmux xclip
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 rm .zshrc
 git clone https://github.com/mazhigali/dotfiles.git
@@ -16,6 +17,5 @@ cp $DOTFILES/init.vim       $HOME/.config/nvim/init.vim
 #ln -s .zshrc       $HOME/.zshrc
 #ln -s init.vim       $HOME/.config/nvim/init.vim
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
