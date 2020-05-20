ecc35d9b2a45e3fd74a657826ca5479139c24a86
mazhigali <mazhigali@gmail.com>
2020-05-20 13:44:01 +0300

    add bitrix backup folder to exclude

--- backuper.sh
+++ backuper.sh
@@ -21,8 +21,9 @@ IGN="$sitedirectory/upload/*"
 IGN2="$sitedirectory/bitrix/cache/*"
 IGN3="$sitedirectory/bitrix/managed_cache/*"
 IGN4="$sitedirectory/bitrix/html_pages/*"
+IGN5="$sitedirectory/bitrix/backup/*"
 
-/bin/tar -czf /tmp/backupFiles-$projectName-$D-$M-$Y.tar.gz --exclude="$IGN" --exclude="$IGN2" --exclude="$IGN3" --exclude="$IGN4" $sitedirectory
+/bin/tar -czf /tmp/backupFiles-$projectName-$D-$M-$Y.tar.gz --exclude="$IGN" --exclude="$IGN2" --exclude="$IGN3" --exclude="$IGN4" --exclude="$IGN5" $sitedirectory
 rclone copy /tmp/backupFiles-$projectName-$D-$M-$Y.tar.gz ya:Backup/$projectName
 /bin/rm -f /tmp/backupFiles-$projectName-$D-$M-$Y.tar.gz
 
